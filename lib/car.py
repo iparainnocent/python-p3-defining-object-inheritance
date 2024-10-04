25bfbb7c48ba0ff3ff2fe9dca32e0776db504be2
innocent <iparainnocent@gmail.com>
2024-10-04 17:23:58 +0300

    commit changes

--- lib/car.py
+++ lib/car.py
@@ -1,4 +1,5 @@
-from vehicle import Vehicle
+from vehicle import Vehicle  # Import the Vehicle class
 
 class Car(Vehicle):
-    pass
+    def go(self):
+        return "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
